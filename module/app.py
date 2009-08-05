2c67538d35186c38b9df3c4c6d071666a7a1c96e
Léon Keijser <keijser@stone-it.com>
2009-08-05 23:38:59 +0200

    Commented out test function since it's not being used atm

--- module/app.py
+++ module/app.py
@@ -126,10 +126,10 @@ def main():
         action="store_true",
         dest="printindex",
         help="Prints index file")
-    parser.add_option("-t", "--test",
-        action="store_true",
-        dest="test",
-        help="Danger, Will Robinson, Danger! test parameter - can do anything! Review source before executing!")
+    #parser.add_option("-t", "--test",
+    #    action="store_true",
+    #    dest="test",
+    #    help="Danger, Will Robinson, Danger! test parameter - can do anything! Review source before executing!")
 
     # parse cmd line options
     (options, args) = parser.parse_args()
@@ -151,7 +151,7 @@ def main():
     s.showserial    = options.showserial
     s.printcert     = options.printcert
     s.printindex    = options.printindex
-    s.test          = options.test
+    #s.test          = options.test
     # values we got from parsing the configuration file:
     s.cacertfile    = cacertfile
     s.cakeyfile     = cakeyfile
@@ -208,7 +208,7 @@ class StoneVPN:
         self.showserial    = None
         self.printcert     = None
         self.printindex    = None
-        self.test          = None
+        #self.test          = None
         # should we do the same for values we got from
         # parsing the configuration file? i don't think
         # so, so let's comment them out for now.
