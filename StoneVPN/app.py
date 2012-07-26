4e7b6456d499ed5632cff05f56808713f54d2296
Léon Keijser <keijser@stone-it.com>
2012-07-26 22:20:19 +0200

    Updated dates in copyright notices.

--- StoneVPN/app.py
+++ StoneVPN/app.py
@@ -2,7 +2,7 @@
 
  StoneVPN - Easy OpenVPN certificate and configuration management
 
- (C) 2009,2010 by L.S. Keijser, <keijser@stone-it.com>
+ (C) 2009-2012 by L.S. Keijser, <keijser@stone-it.com>
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
